034670a50ecfc15e3f5f546e483248df53b8071e
Fabio Kusaba <fabinhokb44@gmail.com>
2026-01-24 19:50:29 -0300

    Modern Angular Course 05: Introducing Signals

--- src/app/hello/hello.ts
+++ src/app/hello/hello.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-hello',
@@ -15,4 +15,20 @@ export class Hello {
     console.log('Button clicked!');
     this.isDisabled = !this.isDisabled;
   }
+
+  protected count = signal(0);
+
+  increaseCounter() {
+    // count = count + 1
+    this.count.update(value => value + 1);
+  }
+
+  decreaseCounter() {
+    this.count.update(value => value - 1);
+  }
+
+  resetCounter() {
+    this.count.set(0);
+  }
 }
+
